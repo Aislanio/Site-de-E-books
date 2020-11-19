3c36806e7eccb34f18fe68e412ebba6ec24d0d74
Aislanio <aislanioamorim@gmail.com>
2020-11-19 19:33:24 -0300

    funcionando o dowload para baixar o livro

--- scripts/Ebooks.js
+++ scripts/Ebooks.js
@@ -8,148 +8,88 @@ const sectionCards1 = document.querySelector(".mark");
 /*script*/ 
 const card = document.querySelector(".card");
 
-const b = 1;
 const videos = [
   {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
+    img:"img/img_Ebooks/ebook1.png",
+    title: "JavaScript O Guia Definitiovo",
+    autor: "David Flanagan",
+    id: "prog/JavaScript O Guia Definitivo_v2.pdf"
   },
   {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
+    img:"img/img_Ebooks/ebook2.png",
+    title: "Apostila Matlab",
+    autor: "Alex Jenaro Becker,...",
+    id: "prog/Apostila_Matlab.pdf"
   },
   {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
+    img:"img/img_Ebooks/ebook3.png",
+    title: "Programaçãoem JAVA ",
+    autor: "Daniela Barreiro Claro,...",
+    id: "prog/0031.pdf"
   },
   {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
+    img:"img/img_Ebooks/ebook4.png",
+    title: "Introdução JavaScript",
+    autor: "Prof. Vilson Heck Junior",
+    id: "prog/04_introducao_javaScript.pdf"
   },
-  {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
-   {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
-  {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
-  {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
-  {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
-  {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
+  
 ];
 videos.map(video => {
-  const cardClone = card.cloneNode(true);
-  cardClone.querySelector("img").src = video.img;
-  cardClone.querySelector(".titulo").innerHTML = video.title;
-  cardClone.querySelector(".resum").innerHTML =video.autor;
-  sectionCards.appendChild(cardClone);
+  const card_clone = card.cloneNode(true);
+  card_clone.querySelector("img").src = video.img;
+  card_clone.querySelector(".titulo").innerHTML = video.title;
+  card_clone.querySelector(".resum").innerHTML =video.autor;
+  card_clone.setAttribute("id", video.id);
+  sectionCards.appendChild(card_clone);
   sectionCards.style.opacity = "1";
 });
 
-
-const mark = [
-  {
-    img:"lalala",
-    title: "2",
-    autor: "22",
-    resumo: "Gyanfkoanfo"
-  },
-  {
-    img:"lalala",
-    title: "2",
-    autor: "22",
-    resumo: "Gyanfkoanfo"
-  },
-  {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
-   {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
-  {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
-  {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
-  {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
-  {
-    img:"lalala",
-    title: "1",
-    autor: "Aislanio",
-    resumo: "Gyanfkoanfo"
-  },
-];
-mark.map(vide => {
-  const cardClon = card.cloneNode(true);
-  card.remove();
-  cardClon.querySelector("img").src = vide.img;
-  cardClon.querySelector(".titulo").innerHTML = vide.title;
-  cardClon.querySelector(".resum").innerHTML =vide.autor;
-  sectionCards1.appendChild(cardClon);
-  
+const modal = document.querySelector(".page");
+const cards = [...document.querySelectorAll(".cards .card")];
+ 
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    modal.querySelector(
+      ".dow"
+    )
+    
+    .href = `ebooks/${card.getAttribute("id")}`;
+    modal.classList.add("active")
+    document.querySelector("body").style.overflow = "hidden";
+  });
 });
+
+document.querySelector(".close_page").addEventListener("click", () => {
+   modal.classList.remove("active");
+   modal.querySelector(".dow").href = ``;
+  document.querySelector("body").style.overflow = "initial";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 window.onscroll = function() {myFunction()};
-
 function myFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.querySelector("header").style.position = "fixed"
   } else {
     document.querySelector("header").style.position = ""
   }  
 }
+
+card.remove()
